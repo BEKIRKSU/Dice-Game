fb36f48619e8b70c48d09ce24a1eff506b818677
Bekir <bekirkscu@gmail.com>
2023-02-02 18:33:16 +0000

    index.js added

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var randomNumber1 = (Math.floor(Math.random() * 6) + 1; //This will give a full number between 1-6
+// If not sure what the code is printing, copy paste it into the console on the web of the desktop version of this file and see 
+//what the values are.
+
+//So we'll get a random number from 1-6 and we'll use this random number to select a random dice image. e.g. dice1 - dice2
+//So we can use contactenation to add this number to the ed of the string 'dice' and get our random dice image.
+
+var randomDiceImage = "dice" + randomNumber1 +".png"; //this is so clean. Imagine, and then code it. Create it in your head and code it. 
+//for example "dice3.png"
